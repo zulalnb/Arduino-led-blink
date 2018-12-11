b28bdc9825b47bc3de6f838a58d917a800b4548d
Zülal Nebin <zllnbn1997@gmail.com>
2018-12-11 21:38:07 +0300

    arduino

---
+++ lednet1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace lednet1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonOn = new System.Windows.Forms.Button();
+            this.buttonOff = new System.Windows.Forms.Button();
+            this.labelleddurum = new System.Windows.Forms.Label();
+            this.pictureBoxled = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelbagdurum = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxled)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOn
+            // 
+            this.buttonOn.Location = new System.Drawing.Point(39, 158);
+            this.buttonOn.Name = "buttonOn";
+            this.buttonOn.Size = new System.Drawing.Size(75, 23);
+            this.buttonOn.TabIndex = 0;
+            this.buttonOn.Text = "LED Yak";
+            this.buttonOn.UseVisualStyleBackColor = true;
+            this.buttonOn.Click += new System.EventHandler(this.buttonOn_Click);
+            // 
+            // buttonOff
+            // 
+            this.buttonOff.Location = new System.Drawing.Point(146, 158);
+            this.buttonOff.Name = "buttonOff";
+            this.buttonOff.Size = new System.Drawing.Size(84, 23);
+            this.buttonOff.TabIndex = 1;
+            this.buttonOff.Text = "LED Söndür";
+            this.buttonOff.UseVisualStyleBackColor = true;
+            this.buttonOff.Click += new System.EventHandler(this.buttonOff_Click);
+            // 
+            // labelleddurum
+            // 
+            this.labelleddurum.AutoSize = true;
+            this.labelleddurum.Location = new System.Drawing.Point(93, 207);
+            this.labelleddurum.Name = "labelleddurum";
+            this.labelleddurum.Size = new System.Drawing.Size(66, 13);
+            this.labelleddurum.TabIndex = 2;
+            this.labelleddurum.Text = "labelledurum";
+            this.labelleddurum.Visible = false;
+            // 
+            // pictureBoxled
+            // 
+            this.pictureBoxled.Location = new System.Drawing.Point(256, 53);
+            this.pictureBoxled.Name = "pictureBoxled";
+            this.pictureBoxled.Size = new System.Drawing.Size(215, 167);
+            this.pictureBoxled.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxled.TabIndex = 3;
+            this.pictureBoxled.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Bağlan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Bağlantıyı Kes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelbagdurum
+            // 
+            this.labelbagdurum.AutoSize = true;
+            this.labelbagdurum.Location = new System.Drawing.Point(93, 127);
+            this.labelbagdurum.Name = "labelbagdurum";
+            this.labelbagdurum.Size = new System.Drawing.Size(76, 13);
+            this.labelbagdurum.TabIndex = 7;
+            this.labelbagdurum.Text = "labelbagdurum";
+            this.labelbagdurum.Visible = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(87, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "PORT:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "BAUDRATE:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 287);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.labelbagdurum);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBoxled);
+            this.Controls.Add(this.labelleddurum);
+            this.Controls.Add(this.buttonOff);
+            this.Controls.Add(this.buttonOn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arduino Led";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxled)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOn;
+        private System.Windows.Forms.Button buttonOff;
+        private System.Windows.Forms.Label labelleddurum;
+        private System.Windows.Forms.PictureBox pictureBoxled;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelbagdurum;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lednet1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace lednet1
+{
+    public partial class Form1 : Form
+    {
+        string[] portlar = SerialPort.GetPortNames();
+        bool LEDDurum = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            //serialPort1 = new SerialPort();
+            //serialPort1.PortName = "COM3";
+            //serialPort1.BaudRate = 9600;
+            //serialPort1.Open();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+            
+        }
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+            labelleddurum.Visible = true;
+            labelleddurum.Text = "LED Yanıyor...";
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+            LEDDurum = true;
+            pictureBoxled.Image = Image.FromFile("C:/Users/istanbul/Desktop/Bullet-green.png");
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("0");
+            labelleddurum.Visible = true;
+            labelleddurum.Text = "LED Söndü...";
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            LEDDurum = false;
+            pictureBoxled.Image = Image.FromFile("C:/Users/istanbul/Desktop/buttonson.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string port in portlar)
+            {
+                comboBox1.Items.Add(port);
+                comboBox1.SelectedIndex = 0;
+            }
+            comboBox2.Items.Add("4800");
+            comboBox2.Items.Add("9600");
+            comboBox2.SelectedIndex=2;
+            labelbagdurum.Visible = true;
+            labelbagdurum.Text = "Bağlantı Kapalı!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                if (comboBox1.Text == "") return;
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt16(comboBox2.Text);
+                try
+                {
+                    serialPort1.Open();
+                    labelbagdurum.ForeColor = Color.Green;
+                    labelbagdurum.Text = "Bağlantı Açık!";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //throw;
+                }
+            }
+            else
+            {
+                labelbagdurum.Text = "Bağlantı Kurulu!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+            labelbagdurum.Visible = true;
+            labelbagdurum.Text="Bağlantı Kapalı!";
+        }
+    }
+}
+
+            //serialPort1.PortName = comboBox1.Text;
+            //serialPort1.Open();
+            //labelbagdurum.Visible = true;
+            //labelbagdurum.Text = "Bağlantı Açık!";           
